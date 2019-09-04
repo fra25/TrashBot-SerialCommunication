1599bdd4083c6dbeec5e2e2c71c87955912afe2b
Federico Gerardi <38018471+PyGera@users.noreply.github.com>
2019-09-04 19:46:31 +0200

    Prova
    Stesso discorso de prima

---
+++ slave.ino
@@ -0,0 +1,29 @@
+#include <Serial.h>
+
+// Al momento facciamo solo accendere dei led
+// In futuro faremo muovere i motori
+
+String data;
+
+void lampeggio(int pinLed = 1) {
+  digitalWrite(pinLed, HIGH);
+  delay(200);
+  digitalWrite(pinLed, LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(1, OUTPUT);
+  digitalWrite(1, LOW);
+}
+
+void loop() {
+  data = Serial.readString()
+  if (data == "1") {
+    lampeggio();
+  }
+  else if (data == "2") {
+    lampeggio();
+    lampeggio();
+  }
+}
