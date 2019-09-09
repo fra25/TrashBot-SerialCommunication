59b577d602c25b2cc5b9980babc087df8a5bc465
Gabriele Onorato <lapry34@gmail.com>
2019-09-09 03:04:27 +0200

    Identificazione motori
    Identificazione posizione motori, formatazzione e aggiunta commenti
    
    AH GIERI

--- slave.ino
+++ slave.ino
@@ -1,12 +1,12 @@
-int pwm_a = 6;  //PWM control for motor outputs 1 and 2
-int pwm_b = 9;  //PWM control for motor outputs 3 and 4
+int pwm_a = 6;  //PWM control 
+int pwm_b = 9;  
 int pwm_c = 5;
 int pwm_d = 3;
 
-int dir_a = 7;  //direction control for motor outputs 1 and 2
-int dir_b = 8;  //direction control for motor outputs 3 and 4
-int dir_c = 11;
-int dir_d = 10;
+int dir_a = 7;  //Motore posteriore destro
+int dir_b = 8;  //Motore anteriore destro
+int dir_c = 11; //Motore posteriore sinistro
+int dir_d = 10; //Motore anteriore sinistro
 
 
 
@@ -19,10 +19,11 @@ void setup(){
 
   pinMode(pwm_a, OUTPUT);  //Set control pins to be outputs
   pinMode(pwm_b, OUTPUT);
-  pinMode(dir_a, OUTPUT);
-  pinMode(dir_b, OUTPUT);
   pinMode(pwm_c, OUTPUT);
   pinMode(pwm_d, OUTPUT);
+  
+  pinMode(dir_a, OUTPUT);
+  pinMode(dir_b, OUTPUT);
   pinMode(dir_c, OUTPUT);
   pinMode(dir_d, OUTPUT);
 
@@ -34,7 +35,7 @@ void loop()
 {
   digitalWrite(dir_a, HIGH);
   digitalWrite(dir_b, HIGH);
-  digitalWrite(dir_c, LOW); //cavo invertito (mannaggia a te)
+  digitalWrite(dir_c, HIGH); 
   digitalWrite(dir_d, HIGH);
 
   String data = Serial.readString();
@@ -42,14 +43,14 @@ void loop()
   if(data == "destra"){
       digitalWrite(dir_a, LOW);   //ruote di destra
       digitalWrite(dir_b, LOW);
-      digitalWrite(dir_c, LOW); //cavo invertito (mannaggia a te)
+      digitalWrite(dir_c, HIGH); 
       digitalWrite(dir_d, HIGH);
-  } else if (data == "sinistra"){
+  } else if(data == "sinistra"){
       digitalWrite(dir_a, HIGH);
       digitalWrite(dir_b, HIGH);
-      digitalWrite(dir_c, HIGH); //ruote di sinistra  //cavo invertito (mannaggia a te)
+      digitalWrite(dir_c, LOW); //ruote di sinistra  
       digitalWrite(dir_d, LOW);
-  } else if( data.toInt() >= 190 && data.toInt() <= 255){
+  } else if(data.toInt() >= 190 && data.toInt() <= 255){
       setVelocity(data.toInt());
   } else if(data == "spegni"){
       setVelocity(0);
@@ -64,7 +65,7 @@ void setVelocity(int v){
   if((v < 190 && v != 0) || v > 255)
     return;
 
-  analogWrite(pwm_a, v);  //set both motors to run at (100/255 = 39)% duty cycle (slow)
+  analogWrite(pwm_a, v);  
   analogWrite(pwm_b, v);  //minimo 190 o non partono wewo
   analogWrite(pwm_c, v);
   analogWrite(pwm_d, v);
